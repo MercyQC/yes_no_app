7ca5126200a7cef78c1af4b51b1c0fb55065b526
MercyQC <20470106@campeche.tecnm.mx>
2024-07-11 09:29:18 -0600

    Mappers

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/Infraestructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer{
 
@@ -8,7 +9,12 @@ class GetYesNoAnswer{
 
     final response = await _dio.get('https://yesno.wtf/assets/yes/14-b57c6dc03aa15a4b18f53eb50d6197ee.gif'); 
 
+final yesNoModel = YesNoModel.fromJsonMap(response.data);  
 
-    throw UnimplementedError(); 
+
+return Message(
+  text: yesNoModel.answer,
+  fromWho: FromWho.hers,
+  imgUrl: yesNoModel.image); 
   }
 } 
